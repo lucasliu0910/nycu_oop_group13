6166dde8b1edfcd9a9d772141492f93a6f7659f9
劉恩佑 EN-YOU LIU <lucasliu0910@gmail.com>
2025-04-23 23:15:23 +0800

    Squashed '250421/' content from commit dad38f8
    git-subtree-dir: 250421
    git-subtree-split: dad38f8e23dd8b51425da66880009baf078844d2

---
+++ custom_gymnasium/custom_gymnasium/envs/custom_lunarlander.py
@@ -0,0 +1,53 @@
+import numpy as np
+import gymnasium as gym
+from typing import TYPE_CHECKING, Optional
+from custom_gymnasium.utils.base_lander import BASE_LANDER
+
+class CustomLunarLander_v1(BASE_LANDER):
+    def __init__(
+        self,
+        render_mode: Optional[str] = None,
+        continuous: bool = False,
+        gravity: float = -10.0,
+        enable_wind: bool = False,
+        wind_power: float = 15.0,
+        turbulence_power: float = 1.5,
+        fuel: float = 100,):
+        pass
+
+        '''
+        Task:
+        - Implement a custom lunar lander environment where the lander has a fuel tank of size `fuel`.
+        '''
+        # =====================type your code here=====================
+        
+        
+        # =============================================================
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,):
+        pass
+        '''
+        Task:
+        - Reset the fuel tank to its original size.
+        '''
+        # =====================type your code here=====================
+        
+        # =============================================================
+        
+    def step(self, action):
+        pass
+        '''
+        Task:
+        - The lander can only move if it has fuel left. If the fuel is exhausted, the lander can no longer move.
+        
+        Hint:
+        - If the lander moves, the fuel tank should be decremented by 1.
+        - If the lander doesn't move, the fuel tank should remain the same.
+        '''
+        # =====================type your code here=====================
+        
+        # =============================================================
